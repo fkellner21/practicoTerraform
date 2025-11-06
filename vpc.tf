854f6a10f74bafd813e850fe4cbaa51138641267
Francisco Kellner <kellner.francisco@gmail.com>
2025-11-06 20:04:20 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -61,7 +61,7 @@ resource "aws_route_table" "public_route_table" {
 
 }
 
-resource "aws_route_table_association" "private_rt_asso" {
+resource "aws_route_table_association" "public_rt_asso" {
   count          = 2
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
   route_table_id = aws_route_table.public_route_table.id
@@ -88,7 +88,7 @@ resource "aws_route_table_association" "private_rt_asso" {
 }
 
 resource "aws_eip" "eip_natgw" {
-  count = 1
+  count = 2
 }
 
 resource "aws_nat_gateway" "natgateway" {
